009b6cf37a12f50096bd63e332fc0590ee147259
Ravish <211128@iiitt.ac.in>
2024-03-14 23:41:15 +0530

    custom news data.json added

---
+++ data.js
@@ -0,0 +1,151 @@
+export const SavedArticles = [
+    {
+        "uuid": "4002b5b4-5999-46b4-aca0-6074bd31b84a",
+        "title": "LCK, 롤파크에 오프라인 서버 도입 '다음부부터 정상화'",
+        "description": "리그오브레전드 챔피언스 코리아(이하 LCK)가 디도스 공격 대응을 위해 오프라인 서버를 도입했다. LCK는 공식 유튜브 채?...",
+        "keywords": "라이엇게임즈, LCK, 리그 오브 레전드, 리그오브레전드, 롤, LoL, 디도스, 공격, 정상화",
+        "snippet": "▲ 출처: LCK 공식 유튜브 채널\n\n리그오브레전드 챔피언스 코리아(이하 LCK)가 디도스 공격 대응을 위해 오프라인 서버를 ?...",
+        "url": "http://www.gameinsight.co.kr/news/articleView.html?idxno=31991",
+        "image_url": "https://cdn.gameinsight.co.kr/news/thumbnail/202403/31991_75835_2735_v150.jpg",
+        "language": "ko",
+        "published_at": "2024-03-13T18:28:27.000000Z",
+        "source": "gameinsight.co.kr",
+        "categories": [],
+        "relevance_score": null
+    },
+    {
+        "uuid": "fa1eab48-6a78-40ca-921a-ae514117622e",
+        "title": "[인터뷰] 나이언틱 존 행키 CEO, “AI과 AR 결합 차세대 플랫폼 주목, 한국 중추적 역할 수행할 것”",
+        "description": "지난 2016년 ‘포켓몬GO’을 출시하며 일약 스타덤에 오른 기업 나이언틱 존 행키 CEO가 방한했다. 나이언틱은 13일 중구 구...",
+        "keywords": "",
+        "snippet": "지난 2016년 ‘포켓몬GO’을 출시하며 일약 스타덤에 오른 기업 나이언틱 존 행키 CEO가 방한했다. 나이언틱은 13일 중구 구...",
+        "url": "https://www.khgames.co.kr/news/articleView.html?idxno=225605",
+        "image_url": "https://cdn.khgames.co.kr/news/thumbnail/202403/225605_251654_267_v150.jpg",
+        "language": "ko",
+        "published_at": "2024-03-13T18:28:08.000000Z",
+        "source": "khgames.co.kr",
+        "categories": [],
+        "relevance_score": null
+    },
+    {
+        "uuid": "6592829f-02d0-4ea9-af1b-9b6b2d29a45b",
+        "title": "Samardzic e l'amicizia con Yildiz: \"Prima dell'Inter sul campetto a Berlino...\"",
+        "description": "Il centrocampista dell'Udinese ha raccontato il proprio percorso professionale e l'amicizia che lo lega al giocatore bianconero",
+        "keywords": "",
+        "snippet": "\"Io e il mio amico Yildiz abbiamo lo stesso preparatore e siamo entrambi nati in Germania. Ognuno di noi poi ha preso strade diverse: io ho scelto di rappresent...",
+        "url": "https://www.tuttosport.com/news/calcio/serie-a/juventus/2024/03/13-124038770/samardzic_e_l_amicizia_con_yildiz_prima_dell_inter_sul_campetto_a_berlino_",
+        "image_url": "https://cdn.tuttosport.com/images/2024/03/13/183631447-2f634806-84a6-4df3-9ac9-4a877df47400.jpg",
+        "language": "it",
+        "published_at": "2024-03-13T18:28:06.000000Z",
+        "source": "tuttosport.com",
+        "categories": [
+            "sports"
+        ],
+        "relevance_score": null
+    },
+    {
+        "uuid": "1d76e930-83a6-45a0-9255-47df0436384b",
+        "title": "South Korea mulls \"future-oriented\" statement with Japan for anniversary",
+        "description": "A senior South Korean official anticipates that leaders of South Korea and Japan may issue a joint statement incorporating future-oriented commitments when the ...",
+        "keywords": "japan south korea, south korea japan, kishida yoon, joint statement kishida yoon, japan south korea 60th anniversary",
+        "snippet": "KYODO NEWS - 20 minutes ago - 18:28 | World, All, Japan\n\nA senior South Korean official anticipates that leaders of South Korea and Japan may issue a joint stat...",
+        "url": "https://english.kyodonews.net/news/2024/03/cfa8b24a0e11-s-korea-mulls-future-oriented-statement-with-japan-for-anniv.html",
+        "image_url": "https://img.kyodonews.net/english/public/images/posts/255f25b5adb8420b8654f8c7c4199d95/cropped_image_l.jpg",
+        "language": "en",
+        "published_at": "2024-03-13T18:28:00.000000Z",
+        "source": "english.kyodonews.net",
+        "categories": [
+            "general"
+        ],
+        "relevance_score": null
+    },
+    {
+        "uuid": "e8f44a33-0d8e-4c17-9a32-01c20ec5f34f",
+        "title": "“국민의힘 비례대표 호남권 몫 약속 지켜라”",
+        "description": "국민의힘의 비례대표 위성정당인 ‘국민의미래’가 비례 공천자 선정을 위한 절차를 진행하는 가운데 지역 내에서 ‘호...",
+        "keywords": "",
+        "snippet": "국민의힘의 비례대표 위성정당인 ‘국민의미래’가 비례 공천자 선정을 위한 절차를 진행하는 가운데 지역 내에서 ‘호...",
+        "url": "http://www.domin.co.kr/news/articleView.html?idxno=1461628",
+        "image_url": "http://www.domin.co.kr/news/thumbnail/202403/1461628_627889_2526_v150.jpg",
+        "language": "ko",
+        "published_at": "2024-03-13T18:28:00.000000Z",
+        "source": "domin.co.kr",
+        "categories": [],
+        "relevance_score": null
+    },
+    {
+        "uuid": "a99273e4-00b8-42c4-857f-41507bb4a5c1",
+        "title": "4.10총선 표심 잡으러 전북 방문 줄이어",
+        "description": "4·10 총선 대진표가 마무리 단계에 접어들면서 정당마다 전북 표심을 잡기 위해 전북으로 향하고 있다.녹색정의당 대표?...",
+        "keywords": "",
+        "snippet": "4·10 총선 대진표가 마무리 단계에 접어들면서 정당마다 전북 표심을 잡기 위해 전북으로 향하고 있다.\n\n녹색정의당 대표...",
+        "url": "http://www.domin.co.kr/news/articleView.html?idxno=1461639",
+        "image_url": "http://www.domin.co.kr/news/thumbnail/202403/1461639_627888_257_v150.jpg",
+        "language": "ko",
+        "published_at": "2024-03-13T18:28:00.000000Z",
+        "source": "domin.co.kr",
+        "categories": [],
+        "relevance_score": null
+    },
+    {
+        "uuid": "5c4ad955-dce5-4727-8623-e82c019e26af",
+        "title": "조국혁신당 바람에 전북 총선판의 변화 예고",
+        "description": "조국혁신당의 바람이 점차 거세지면서 전북 총선판의 변화가 예고되고 있다.조국혁신당 조국 대표의 전북에 대한 애정?...",
+        "keywords": "",
+        "snippet": "조국 조국혁신당 대표가 13일 전북 전주시 전주한옥마을을 방문해 시민들과 인사를 나누고 있다. 특별취재반\n\n조국혁신?...",
+        "url": "http://www.domin.co.kr/news/articleView.html?idxno=1461642",
+        "image_url": "http://www.domin.co.kr/news/thumbnail/202403/1461642_627887_2350_v150.jpg",
+        "language": "ko",
+        "published_at": "2024-03-13T18:28:00.000000Z",
+        "source": "domin.co.kr",
+        "categories": [],
+        "relevance_score": null
+    },
+    {
+        "uuid": "dedabffc-f82a-4ed6-843b-ebb111da8ec1",
+        "title": "Nate Diaz, Jorge Masvidal ditch MMA for rematch in boxing June 1",
+        "description": "Former UFC combatants Nate Diaz and Jorge Masvidal will fight again June 1 at the Kia Forum in Inglewood, California, but this time in a boxing match, officials...",
+        "keywords": "",
+        "snippet": "Jake Paul and Nate Diaz go the distance, but Paul ends up the victor in Diaz's boxing debut. (2:01)\n\nOpen Extended Reactions\n\nA rematch of a marquee UFC headlin...",
+        "url": "https://www.espn.com/boxing/story/_/id/39723699/nate-diaz-jorge-masvidal-ditch-mma-rematch-boxing-june-1",
+        "image_url": "https://a.espncdn.com/combiner/i?img=/photo/2019/1030/r620806_1296x729_16-9.jpg",
+        "language": "en",
+        "published_at": "2024-03-13T18:27:42.000000Z",
+        "source": "espn.com",
+        "categories": [
+            "sports",
+            "general"
+        ],
+        "relevance_score": null
+    },
+    {
+        "uuid": "257117f0-6d65-4e05-95e3-1735b7847f39",
+        "title": "广西强化纠纷多元化解 挽回消费者损失1.4亿元-中新网",
+        "description": "",
+        "keywords": "消费维权, 查办案件, 市场监督, 消费升级, 万村",
+        "snippet": "中新网南宁3月13日电(林浩)3月13日，广西壮族自治区市场监督管理局一级巡视员李昌华介绍，2023年，广西强化纠纷多元化?...",
+        "url": "https://www.chinanews.com.cn/cj/2024/03-13/10179654.shtml",
+        "image_url": "https://i2.chinanews.com.cn/simg/ypt/2024/240312/736a9872-ca2f-4356-9f34-92b578d47e8b_zsite_sl.jpg",
+        "language": "zh",
+        "published_at": "2024-03-13T18:27:34.000000Z",
+        "source": "chinanews.com",
+        "categories": [],
+        "relevance_score": null
+    },
+    // {
+    //     "uuid": "0704219c-c38e-4291-9da5-e934948a7b8a",
+    //     "title": "Q3 2023 PearlAbyss Corp Earnings Call Transcript",
+    //     "description": "Nov 08, 2023 / 11:00PM GMTOperator [Interpreted] Good morning, and good evening. First of all, thank you all for joining this conference call. And now we will b",
+    //     "keywords": "GuruFocus, Article, News, GuruFocus Research, XKRX:263750",
+    //     "snippet": "Nov 08, 2023 / 11:00PM GMT\n\nOperator\n\n\n\n[Interpreted] Good morning, and good evening. First of all, thank you all for joining this conference call. And now we w...",
+    //     "url": "https://www.gurufocus.com/news/2382792/q3-2023-pearlabyss-corp-earnings-call-transcript",
+    //     "image_url": "https://static.gurufocus.com/images/global_logo_twitter_card.png",
+    //     "language": "en",
+    //     "published_at": "2024-03-13T18:27:32.000000Z",
+    //     "source": "gurufocus.com",
+    //     "categories": [
+    //         "business"
+    //     ],
+    //     "relevance_score": null
+    // }
+]
