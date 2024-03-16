a399773499eeb4acbf7e182b5502dc09036bc8ff
Ravish <211128@iiitt.ac.in>
2024-03-17 01:48:27 +0530

    Update ProfileCard component styling

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -45,7 +45,7 @@ const ProfileCard = React.forwardRef(({ children }, ref) => {
             handler={handleOpen}
             className="bg-transparent shadow-none"
         >
-            <Card className="w-96 mx-auto">
+            <Card className="w-72 sm:w-96 mx-auto">
                 <CardHeader floated={false} className="h-80">
                     <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" alt="profile-picture" />
                 </CardHeader>
